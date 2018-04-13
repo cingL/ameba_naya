4f7714370e1abe513dc22ddee647477c9ffd6c98
ching <cingl@foxmail.com>
2018-04-13 14:52:12 +0800

    the other way to search img, detail in code

--- crawl_image.py
+++ crawl_image.py
@@ -1,18 +1,18 @@
 import codecs
 import os
+import time
 
 import requests
-import time
 from bs4 import BeautifulSoup
 
 from crawl_url import download_page
-
-
 # get all passage urls from blog_link.txt
+from param import blog_link_txt_name
+
+
 def get_url():
     links = {}
-    # with codecs.open('blog_link.txt', 'rb', 'utf-8') as f:
-    with codecs.open('blog_link_180116.txt', 'rb', 'utf-8') as f:
+    with codecs.open(blog_link_txt_name, 'rb', 'utf-8') as f:
         for line in f:
             # print(line.split(',')[0])
             arr = line.split(',')
@@ -33,24 +33,25 @@ def crawl(url, prefix):
     if not os.path.exists(time):
         os.mkdir(time)
     # named image with title and index
-    all_a = soup.find_all('a', attrs={'class': 'detailOn'})
-    for index in range(len(all_a)):
-        a = all_a[index]
-        img_link = a.find('img')['src']
-        img_path = './' + time + '/' + prefix[5:] + '-' + (index + 1).__str__() + '.jpg'
-        print('img_name = ' + img_path)
-        print('img_link = ' + img_link)
-        img_r = requests.get(img_link, stream=True)
-        if img_r.status_code == 200:
-            with open(img_path, 'wb') as f:
-                for chunk in img_r.iter_content(1024):
-                    f.write(chunk)
+    # all_a = soup.find_all('a', attrs={'class': 'detailOn'}) -> soup读出来找不到了，懒加载了？
+    all_a = soup.find_all('a')
+    index = 0
+    for each_a in all_a:
+        img = each_a.find('img')
+        if img is not None:
+            img_link = img['src']
+            if img_link[-3:] == '800':
+                index += 1
+                img_path = './' + time + '/' + prefix[5:] + '-' + index.__str__() + '.jpg'
+                print('img_name = ' + img_path)
+                print('img_link = ' + img_link)
+                img_r = requests.get(img_link, stream=True)
+                if img_r.status_code == 200:
+                    with open(img_path, 'wb') as f:
+                        for chunk in img_r.iter_content(1024):
+                            f.write(chunk)
 
 
-"""
- cost = 36:30
- total 202 post
-"""
 if __name__ == '__main__':
 
     s_time = time.time()
--- crawl_url.py
+++ crawl_url.py
@@ -1,11 +1,11 @@
 import codecs
+import datetime
 import re
 
-import datetime
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://search.ameba.jp/search.html?q=%E3%80%90%E7%B4%8D%E8%B0%B7%E5%81%A5%E3%80%91&target=id&aid=patch-west&author=all&row=10&prevRow=10&profileRow="
+import param
 
 
 def download_page(url):
@@ -21,22 +21,24 @@ def parse_html(html):
     for blog_li in blog_list_soup.find_all('li'):
         update_array = re.split('\D', blog_li.find('span', attrs={'class': 'updateTime'}).getText())
         update_time = datetime.date(int(update_array[1]), int(update_array[2]), int(update_array[3]))
-        if update_time > datetime.date(2017, 10, 31):
+        if update_time > param.latest_date:
             a = blog_li.find('a', attrs={'class': 'areaClick'})
             link = a['href']
             title = a.find('h3').getText()
             blog_list_urls.append(link + "," + title + " ," + update_time.__str__())
+        else:
+            return blog_list_urls, None
 
     # get next page
-    # next_page = soup.find('div', attrs={'class': 'cmnPaging'}).find('a', attrs={'class': 'fwd'})
-    # if next_page:
-    #     return blog_list_urls, 'https:' + next_page['href']
+    next_page = soup.find('div', attrs={'class': 'cmnPaging'}).find('a', attrs={'class': 'fwd'})
+    if next_page:
+        return blog_list_urls, 'https:' + next_page['href']
     return blog_list_urls, None
 
 
 if __name__ == '__main__':
-    current_url = URL
-    with codecs.open('blog_link_180116.txt', 'wb', encoding='utf-8') as fp:
+    current_url = param.URL
+    with codecs.open(param.blog_link_txt_name, 'wb', encoding='utf-8') as fp:
         while current_url:
             current_html = download_page(current_url)
             urls, current_url = parse_html(current_html)
