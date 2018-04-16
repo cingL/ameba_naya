765d0afee567ef04a305d5ced74bb15dd1429533
ching <cingl@foxmail.com>
2018-04-16 18:09:11 +0800

    arrange

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# ameblo_zaiki
+modify param.py
+
+URL → blog list page
+
+blog_link_txt_name → file storage all blog link, generate after crawl_url.py run
+
+latest_date → only crawl the blog later this date
+
 # ameblo_naya
 get all images from naya's ameba
 
--- param.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-# ameblo search line
-URL = "https://search.ameba.jp/search.html?q=%E3%80%90%E7%B4%8D%E8%B0%B7%E5%81%A5%E3%80%91&target=id&aid=patch-west&author=all&row=10&prevRow=10&profileRow="
-# link txt
-blog_link_txt_name = 'blog_link_180413.txt'
-# date
-latest_date = datetime.date(2018, 1, 15)
